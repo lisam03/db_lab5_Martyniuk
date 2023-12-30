fdd780b1e42a49938dadd61b096590a732d1ff7b
lisam03 <91112587+lisam03@users.noreply.github.com>
2023-12-30 23:14:38 +0200

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -1,57 +1,87 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-# Шлях до CSV-файлу
-csv_constructors_path = "constructors.csv"
-csv_drivers_path = "drivers.csv"
-csv_races_path = "races.csv"
-csv_results_path = "results.csv"
-
-# Завантажити дані з CSV-файлу в Pandas DataFrame
-constructors_data = pd.read_csv(csv_constructors_path)
-drivers_data = pd.read_csv(csv_drivers_path)
-races_data = pd.read_csv(csv_races_path)
-results_data = pd.read_csv(csv_results_path)
-
-# Перейменувати стовпець 'id' на 'constructors_id'
-constructors_data = constructors_data.rename(columns={'id': 'constructors_id'})
-drivers_data = drivers_data.rename(columns={'id': 'drivers_id'})
-races_data = races_data.rename(columns={'id': 'races_id'})
-results_data = results_data.rename(columns={'id': 'results_id'})
-
-# Список умов
-id_drivers = [830, 815, 4, 832, 1, 840, 847, 822, 842, 848, 852, 858, 825, 856, 807, 855, 846, 839, 844, 857]
-id_constructors = [1, 3, 6, 9, 51, 117, 131, 210, 213, 214]
-id_race = [1098, 1099, 1100, 1101, 1102, 1103, 1104, 1105, 1106, 1107, 1108, 1109, 1110, 1111, 1112, 1113, 1114, 1115, 1116, 1117, 1118, 1119, 1120]
-race_year = 2023
-
-# Вибрати конкретні стовпці для подальшого запису
-constructors_selected_columns = ['constructorId', 'name']
-drivers_selected_columns = ['driverId', 'number', 'forename', 'surname', 'nationality']
-races_selected_columns = ['raceId', 'year', 'name']
-results_selected_columns = ['resultId', 'raceId', 'driverId', 'constructorId', 'position', 'points']
-
-# Відфільтрувати дані за id
-constructors_filtered_data = constructors_data[constructors_data['constructorId'].isin(id_constructors)]
-drivers_filtered_data = drivers_data[drivers_data['driverId'].isin(id_drivers)]
-races_filtered_data = races_data[races_data['year'] == 2023]
-results_filtered_data = results_data[results_data['raceId'].isin(id_race)]
-
-# Обрати лише певні стовпці
-constructors_filtered_data = constructors_filtered_data[constructors_selected_columns]
-drivers_filtered_data = drivers_filtered_data[drivers_selected_columns]
-races_filtered_data = races_filtered_data[races_selected_columns]
-results_filtered_data = results_filtered_data[results_selected_columns]
+import csv
+import psycopg2
+from psycopg2 import sql
 
 # З'єднання з базою даних PostgreSQL
-database_url = 'postgresql://postgres:111@localhost:5432/f1'
-engine = create_engine(database_url)
+username = 'postgres'
+password = '111'
+database = 'f1'
+host = 'localhost'
+port = '5432'
 
-# Записати відфільтровані та вибрані стовпці дані в базу даних PostgreSQL
-constructors_filtered_data.to_sql('constructors', engine, if_exists='replace', index=False)
-drivers_filtered_data.to_sql('drivers', engine, if_exists='replace', index=False)
-races_filtered_data.to_sql('races', engine, if_exists='replace', index=False)
-results_filtered_data.to_sql('results', engine, if_exists='replace', index=False)
+conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
 
-# Закрити з'єднання
-engine.dispose()
+# Створення курсора
+cur = conn.cursor()
+
+# Шлях до CSV-файлу
+csv_constructors_path = "D:\КПІ\Вступ до баз даних\lr5\data\constructors.csv"
+csv_drivers_path = "D:\КПІ\Вступ до баз даних\lr5\data\drivers.csv"
+csv_races_path = "D:\КПІ\Вступ до баз даних\lr5\data\\races.csv"
+csv_results_path = "D:\КПІ\Вступ до баз даних\lr5\data\\results.csv"
+
+clear_query = '''
+DELETE FROM results;
+DELETE FROM constructors;
+DELETE FROM drivers;
+DELETE FROM races;
+'''
+
+cur.execute(clear_query)
+
+# Відкриття CSV-файлу та імпорт даних в кожну таблицю drivers
+with open(csv_drivers_path, 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)  # Пропустити заголовок CSV-файлу
+    for row in csv_reader:
+        # Вибірка даних зі строки для подальшої вставки
+        driverId, driverRef, number, code, forename, surname, dob, nationality, url = row
+
+        # Вставка даних в таблицю
+        cur.execute("INSERT INTO drivers (driver_id, driver_forename, driver_surname, driver_number, driver_nationality) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (driver_id) DO NOTHING",
+                    (driverId, forename, surname, number, nationality))
+
+# Відкриття CSV-файлу та імпорт даних в кожну таблицю constructors
+with open(csv_constructors_path, 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)  # Пропустити заголовок CSV-файлу
+    for row in csv_reader:
+        # Вибірка даних зі строки для подальшої вставки
+        constructorId, constructorRef, name, nationality, url = row
+
+        # Вставка даних в таблицю
+        cur.execute("INSERT INTO constructors (constructor_id, constructor_name) VALUES (%s, %s) ON CONFLICT (constructor_id) DO NOTHING",
+                    (constructorId, name))
+
+# Відкриття CSV-файлу та імпорт даних в кожну таблицю races
+with open(csv_races_path, 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)  # Пропустити заголовок CSV-файлу
+    for row in csv_reader:
+        # Вибірка даних зі строки для подальшої вставки
+        raceId, year, round, circuitId, name, date, time, url, fp1_date, fp1_time, fp2_date, fp2_time, fp3_date, fp3_time, quali_date, quali_time, sprint_date, sprint_time = row
+
+        # Вставка даних в таблицю
+        if int(raceId) >= 1098:
+            cur.execute("INSERT INTO races (race_id, race_year, race_name) VALUES (%s, %s, %s) ON CONFLICT (race_id) DO NOTHING",
+                        (raceId, year, name))
+
+# Відкриття CSV-файлу та імпорт даних в кожну таблицю results
+with open(csv_results_path, 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)  # Пропустити заголовок CSV-файлу
+    for row in csv_reader:
+        # Вибірка даних зі строки для подальшої вставки
+        resultId, raceId, driverId, constructorId, number, grid, position, positionText, positionOrder, points, laps, time, milliseconds, fastestLap, rank, fastestLapTime, fastestLapSpeed, statusId = row
+
+        # Додайте умову, щоб зберігалися лише значення result_id, які починаються з 25846
+        if int(raceId) >= 1098:
+            cur.execute(
+                "INSERT INTO results (result_id, race_id, driver_id, constructor_id, final_position, points) VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (result_id) DO NOTHING",
+                (resultId, raceId, driverId, constructorId, position, points))
+
+
+# Збереження змін та закриття курсора та з'єднання
+conn.commit()
+cur.close()
+conn.close()
